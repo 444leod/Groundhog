3a67e7912895afca91acf4c0b247bdffabc05772
Thibault LONGUEPEE <thibault.longuepee@epitech.eu>
2024-04-12 14:46:47 +0200

    style(parameters): removed some useless do's

--- src/Parameters.hs
+++ src/Parameters.hs
@@ -18,19 +18,19 @@ import System.Environment (getArgs)
 parsePeriod :: String -> Maybe Int -> IO Int
 parsePeriod s (Just p)
     | p > 0 = return p
-    | otherwise = do
-        hPutStrLn stderr $ "Cannot accept negative or null value " ++ s
-        exitWith $ ExitFailure 84
-parsePeriod s Nothing = do
-    hPutStrLn stderr $ "Given argument is not a natural number: " ++ s
-    exitWith $ ExitFailure 84
+    | otherwise =
+        (hPutStrLn stderr $ "Cannot accept negative or null value " ++ s) >>
+        exitWith (ExitFailure 84)
+parsePeriod s Nothing =
+    hPutStrLn stderr ("Given argument is not a natural number: " ++ s) >>
+    exitWith (ExitFailure 84)
 
 parseArgs :: [String] -> IO Int
 parseArgs [s] = parsePeriod s $ readMaybe s
-parseArgs xs = do
-    hPutStrLn stderr $ "Wrong argument count. "
-        ++ (show $ length xs) ++ " arguments given. 1 awaited."
-    exitWith $ ExitFailure 84
+parseArgs xs =
+    hPutStrLn stderr ("Wrong argument count. "
+        ++ (show $ length xs) ++ " arguments given. 1 awaited.") >>
+    exitWith (ExitFailure 84)
 
 getPeriod :: IO Int
 getPeriod = do
